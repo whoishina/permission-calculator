0e3159f444cd73dfac39d142166858e6d253f569
Whoishina <animejapan.hn@gmail.com>
2020-11-29 23:44:27 +0700

    edit README.md

--- README.md
+++ README.md
@@ -28,11 +28,24 @@ Lets take a look you will understand :3
     npm i percal
 ```
 
-#### Require as module
-
+## Example code
 ```js
-    const {isHasPermission, getArrayPermission, toHexArray } = require("percal")
+    const {isHasPermission, getArrayPermission, toHexArray } = require("../percal")
+
+    const PAGE_PERMISSIONS_REQUIRE = {
+
+        ADMINISTRATION : 0x80, 
+        CREATE_POST : 0x10,
+        CREATE_PAGE : 0x20,
+        CREATE_USER : 0x40,
+
+    }
+
+    const USER_PERMISSION_CODE = 496
+
+    console.log('USER PERMISSION ARRAY ', getArrayPermission(USER_PERMISSION_CODE))
+
+    console.log("PAGE PERMISSION ARRAY", toHexArray(PAGE_PERMISSIONS_REQUIRE) )
+
+    console.log('ENGOUGH PERMISSION ?', isHasPermission(PAGE_PERMISSIONS_REQUIRE , USER_PERMISSION_CODE ) )
 ```
-
-
-
--- test/index.test.js
+++ test/index.test.js
@@ -2,18 +2,17 @@ const {isHasPermission, getArrayPermission, toHexArray } = require("../percal")
 
 const PAGE_PERMISSIONS_REQUIRE = {
 
-    ADMINISTRATION : 0x8,
+    ADMINISTRATION : 0x80, 
     CREATE_POST : 0x10,
     CREATE_PAGE : 0x20,
     CREATE_USER : 0x40,
-    CHANGE_PASSWORD: 0x80,
-    GET_PASSWORD : 0x40000,
 
 }
 
+const USER_PERMISSION_CODE = 496
 
-console.log('Danh sách quyền của người dùng ', getArrayPermission(2147483647))
+console.log('USER PERMISSION ARRAY ', getArrayPermission(USER_PERMISSION_CODE))
 
-console.log("Các quyền cần thiết ", toHexArray(PAGE_PERMISSIONS_REQUIRE) )
+console.log("PAGE PERMISSION ARRAY", toHexArray(PAGE_PERMISSIONS_REQUIRE) )
 
-console.log('Có đù quyền hay không ?', isHasPermission(PAGE_PERMISSIONS_REQUIRE , 2147483647 ) )
+console.log('ENGOUGH PERMISSION ?', isHasPermission(PAGE_PERMISSIONS_REQUIRE , USER_PERMISSION_CODE ) )
